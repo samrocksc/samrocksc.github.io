94f10a6807bf4732fd7e41d0617ab14d937ba21e
Sam Clark <samrocksc@gmail.com>
2013-05-05 15:51:59 -0400

    solidifying

--- scripts/menu.js
+++ scripts/menu.js
@@ -14,12 +14,12 @@ $(function(){
 /*loading ajax function*/
 $(document).ready(function(){
   $(".resume").click(function(){
-    $("#content").load("data/resume.html");
+        $("#content").load("data/resume.html");
   });
   $(".aboutsam").click(function(){
-     $("#content").load("data/about.html"); 
+        $("#content").load("data/about.html"); 
   });
     $(".home").click(function(){
-     $("#content").load("data/home.html"); 
+        $("#content").load("data/home.html"); 
   });
 });
