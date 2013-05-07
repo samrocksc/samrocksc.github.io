c4519255f1d1ed423cdfad2d0acd4a933e4910fb
Sam Clark <samrocksc@gmail.com>
2013-05-07 17:39:08 -0400

    tweets

--- scripts/menu.js
+++ scripts/menu.js
@@ -30,26 +30,18 @@ $(document).ready(function(){
   });
 });
 /*Twitter API get*/
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
+    var k = "http://api.twitter.com/1/statuses/user_timeline.json?screen_name=samrocksc&include_rts=1&count=5&jsoncallback=";
+
+    $.ajax({
+        dataType: 'jsonp',
+        url: k,
+        success: function (data) {
+            console.log(data);
+            $.each(data, function (i, item) {
+                $("#tweetFeed").append("<div class='tweetCloud'><div id='tweetArrow'></div><div id='tweetText'>" + item.text + "</div></div>");
+            })
+        }
+    });
 });
-
-// Load Twit
-window.onload = function() {
-    var ajax_load            = "<img class='twit-loader' src='img/loading.gif' alt='Loading...'>",
-        twitter_preferences  = {
-            count    : [5],
-            username : '[samrocksc]'
-            retweets : [false, true],
-            replies  : [false, true]
-        },
-        twitterUrl = 'http://twitter.com/statuses/user_timeline.json?screen_name=' + twitter_preferences.username + '&callback=twitterCallback&count=' + twitter_preferences.count + '&include_rts=' + twitter_preferences.retweets + '&exclude_replies=' + twitter_preferences.replies;
-        script     = document.createElement('script');
-
-    $("#twitter_feed").html(ajax_load);
-
-    script.setAttribute('src', twitterUrl);
-    script.setAttribute('async', true);
-
-    document.body.appendChild(script);
-};
