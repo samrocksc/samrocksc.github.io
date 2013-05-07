6038c7859bce5ecbcbf37a2ada5f3bfd072d603c
Sam Clark <samrocksc@gmail.com>
2013-05-07 17:28:58 -0400

    working with tweets

--- scripts/menu.js
+++ scripts/menu.js
@@ -29,3 +29,27 @@ $(document).ready(function(){
      $('#content').load("data/fantasy.html"); 
   });
 });
+/*Twitter API get*/
+$(document).ready(function(){
+    
+});
+
+// Load Twit
+window.onload = function() {
+    var ajax_load            = "<img class='twit-loader' src='img/loading.gif' alt='Loading...'>",
+        twitter_preferences  = {
+            count    : [5],
+            username : '[samrocksc]'
+            retweets : [false, true],
+            replies  : [false, true]
+        },
+        twitterUrl = 'http://twitter.com/statuses/user_timeline.json?screen_name=' + twitter_preferences.username + '&callback=twitterCallback&count=' + twitter_preferences.count + '&include_rts=' + twitter_preferences.retweets + '&exclude_replies=' + twitter_preferences.replies;
+        script     = document.createElement('script');
+
+    $("#twitter_feed").html(ajax_load);
+
+    script.setAttribute('src', twitterUrl);
+    script.setAttribute('async', true);
+
+    document.body.appendChild(script);
+};
