5b1d0ca868119dd807f61c7929ed510abb516e86
Sam Clark <samrocksc@gmail.com>
2013-05-09 13:41:09 -0400

    blah

--- scripts/menudata.js
+++ scripts/menudata.js
@@ -1,4 +1,4 @@
-    <script type="text/javascript">
+$(document).(ready(function(){
         var source = document.getElementById("myTemplate").innerHTML;
         var template = Handlebars.compile(source);
         var data = {
@@ -13,4 +13,4 @@
             ]
         };
         document.getElementById("placeholder").innerHTML = template(data);
-    </script>
+});
