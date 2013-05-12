17a6bdeb99c2658a116c123466758b074e282030
Sam Clark <samrocksc@gmail.com>
2013-05-12 09:53:33 -0700

    blah

--- scripts/template.js
+++ scripts/template.js
@@ -1,24 +1,17 @@
-<script src="scripts/template.js" id="myTemplate" type="text/x-handlebars-template">
-        <div id="container">
-        {{#header}}<div id="header"><a href="{{href}}">{{name}}</a></div>{{/header}}
-        <div id="left-column">
-        <ul id="menu">
-        {{#links}}
-        <li><a href="{{href}}">{{name}}</a>
-        <ul>{{#each sublinks}}<li><a href="{{href}}">{{name}}</a> </li>{{/each}}</ul>
-        </li>
-        {{/links}}
-        </ul>
-        </div>
-        <div id="content">
-            <h2>shoutinginfrench.com</h2>
-            <p>
-                This page is being designed as a self-built website to re-learn web design by Sam Clark.  The goal is to be able to learn how to do all of the web-design involved in the creation of a website.
-            </p>
-            <p>Emphasis is being placed on the usage of javascript and jQuery to re learn web-design.
-            </p>
-        </div>
-        <div id="footer"><a href="http://www.shoutinginfrench.com">2012 shoutinginfrench</a> || Sam Clark || <a href="mailto:samrocksc@gmail.com?Subject=shoutinginfrench.com">samrocksc@gmail.com</a></div>
-        </div>
-        </div>    
-</script>
+        var source = document.getElementById("myTemplate").innerHTML;
+        var template = Handlebars.compile(source);
+        var data = {
+            links: [
+            { "name": "Home", "href": "index.html"},
+            { "name": "About", "href": "#", "sublinks" : [{"name": "Sam", "href": "about.html"}, {"name": "Resume", "href": "resume.html"}, {"name": "Businesses", "href": "businesses.html"}]},
+            { "name": "Blog", "href": "blog.html" },
+            { "name": "Writings", "href": "#", "sublinks" : [{"name": "Fantasy", "href": "fantasy.html"}, {"name": "Rants", "href": "rants.html"}, {"name": "Coffee", "href": "coffee.html"}] },
+            { "name": "Business", "href": "business.html", "sublinks" : [{"name": "Mud Dauber Coffee", "href": "muddauber.html"}, {"name": "Lo Fi Coffee", "href": "lofi.html"}] },
+            { "name": "Links", "href": "links.html" },
+            { "name": "Contact", "href": "contact.html" },
+            ],
+            header: [
+            {"name": "shoutinginfrench", "href": "index.html"},
+            ]
+        };
+        document.getElementById("placeholder").innerHTML = template(data);
