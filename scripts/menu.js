07594f540f0f43df26b71dc76a53635c41022d28
Sam Clark <samrocksc@gmail.com>
2013-05-12 13:40:22 -0700

    hungry again

---
+++ scripts/menu.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    $( ".tumblr" ).embedTumblr("http://api.tumblr.com/v2/blog/shoutinginfrench.tumblr.com/posts?api_key=BLWUwRnzhaCy4zlibEnhQ8qn4BTKiWFnG73oxTUvWgh434jIDM");
+})
