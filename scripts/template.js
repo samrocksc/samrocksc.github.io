dc5940e1c69f01fb0db8e1bc20f9e93ac6cc0633
Sam Clark <samrocksc@gmail.com>
2013-05-12 10:12:02 -0700

    blah

--- scripts/template.js
+++ scripts/template.js
@@ -11,7 +11,7 @@
             { "name": "Contact", "href": "contact.html" },
             ],
             header: [
-            {"name": "<h1>s h o u t i n g i n f r e n c h</h1>", "href": "index.html"},
+            {"name": "s h o u t i n g i n f r e n c h", "href": "index.html"},
             ]
         };
         document.getElementById("placeholder").innerHTML = template(data);
