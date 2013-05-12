5a5a5afedf9e6a96e3de7a9340a419392964d215
Sam Clark <samrocksc@gmail.com>
2013-05-12 12:42:51 -0700

    changing some css

--- scripts/template.js
+++ scripts/template.js
@@ -2,17 +2,21 @@
         var template = Handlebars.compile(source);
         var data = {
             links: [
-            { "class": "accordionLink", "title": "", "content": "<a href='index.html'>Home</a>"},
-            { "class": "accordionButton", "title": "", "content": "About"},
-            { "class": "accordionContent", "title": "", "content": "<a href='about.html'>Sam</a><br/><a href='resume.html'>Resume</a><br/><a href'businesses.html'>Businesses</a>"},
-            { "class": "accordionLink", "title": "", "content": "<a href='blog.html'>Blog</a>"},
-            { "class": "accordionButton", "title": "", "content": "Writings"},
-            { "class": "accordionContent", "title": "", "content": "<a href='about.html'>Rants</a><br/><a href='resume.html'>Coffee</a><br/><a href'businesses.html'>Fantasy</a>"},
-            { "class": "accordionLink", "title": "", "content": "<a href='index.html'>Links</a>"},
-            { "class": "accordionLink", "title": "", "content": "<a href='index.html'>Contact</a>"},
+                { "class": "accordionLink", "title": "", "content": "<a href='index.html'>Home</a>"},
+                { "class": "accordionButton", "title": "", "content": "About"},
+                { "class": "accordionContent", "title": "", "content": "<a href='about.html'>Sam</a><br/><a href='resume.html'>Resume</a><br/><a href'businesses.html'>Businesses</a>"},
+                { "class": "accordionLink", "title": "", "content": "<a href='blog.html'>Blog</a>"},
+                { "class": "accordionButton", "title": "", "content": "Writings"},
+                { "class": "accordionContent", "title": "", "content": "<a href='about.html'>Rants</a><br/><a href='resume.html'>Coffee</a><br/><a href'businesses.html'>Fantasy</a>"},
+                { "class": "accordionLink", "title": "", "content": "<a href='index.html'>Links</a>"},
+                { "class": "accordionLink", "title": "", "content": "<a href='index.html'>Contact</a>"},
             ],
             header: [
-            {"name": "s h o u t i n g i n f r e n c h", "href": "index.html"},
-            ]
+                {"name": "s h o u t i n g i n f r e n c h", "href": "index.html"},
+            ],
+            footer: [
+                {"content": "
+        <a href='http://www.shoutinginfrench.com'>2012 shoutinginfrench</a> || Sam Clark || <a href='mailto:samrocksc@gmail.com?Subject=shoutinginfrench.com'>samrocksc@gmail.com</a>"}
+            ].
         };
         document.getElementById("placeholder").innerHTML = template(data);
