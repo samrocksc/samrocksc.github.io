1d888fc75f14e3c123bd6a7babac6ca4c986e724
Sam Clark <samrocksc@gmail.com>
2013-05-17 14:42:29 -0400

    playing with auto resize

--- scripts/menu.js
+++ scripts/menu.js
@@ -47,3 +47,8 @@ $(document).ready(function() {
 $(document).ready(function(){
     $( ".tumblr" ).embedTumblr("http://api.tumblr.com/v2/blog/shoutinginfrench.tumblr.com/posts?api_key=BLWUwRnzhaCy4zlibEnhQ8qn4BTKiWFnG73oxTUvWgh434jIDM");
 });
+
+/*resizable code examples*/
+$(document).ready(function(){
+    $('.example').autoResize(); 
+});
