de04eaf972473293d75860abdb0ac0dece932577
Sam Clark <samrocksc@gmail.com>
2013-05-19 16:01:32 -0400

    fixing jquery problem

--- scripts/template.js
+++ scripts/template.js
@@ -12,23 +12,23 @@
             
                 
             links: [
-            { "class": "accordionLink", "title": "", "content": "<a href='index.html'>Home</a>"},
+            { "class": "accordionLink", "title": "", "content": "<a href='/index.html'>Home</a>"},
             { "class": "accordionButton", "title": "", "content": "About"},
-            { "class": "accordionContent", "title": "", "content": "<a href='about.html'>Sam</a><br/><a href='resume.html'>Resume</a><br/><a href='businesses.html'>Businesses</a>"},
-            { "class": "accordionLink", "title": "", "content": "<a href='blog.html'>Blog</a>"},
+            { "class": "accordionContent", "title": "", "content": "<a href='/about.html'>Sam</a><br/><a href='/resume.html'>Resume</a><br/><a href='/businesses.html'>Businesses</a>"},
+            { "class": "accordionLink", "title": "", "content": "<a href='/blog.html'>Blog</a>"},
             { "class": "accordionButton", "title": "", "content": "Writings"},
-            { "class": "accordionContent", "title": "", "content": "<a href='code'>Code</a><br/><a href='rants.html'>Rants</a><br/><a href='coffee.html'>Coffee</a><br/><a href='fantasy.html'>Fantasy</a>"},
-            { "class": "accordionLink", "title": "", "content": "<a href='links.html'>Links</a>"},
-            { "class": "accordionLink", "title": "", "content": "<a href='mailto:samrocksc@gmail.com?Subject=shoutinginfrench.com'>Contact</a>"},
+            { "class": "accordionContent", "title": "", "content": "<a href='/code'>Code</a><br/><a href='/rants.html'>Rants</a><br/><a href='/coffee.html'>Coffee</a><br/><a href='/fantasy.html'>Fantasy</a>"},
+            { "class": "accordionLink", "title": "", "content": "<a href='/links.html'>Links</a>"},
+            { "class": "accordionLink", "title": "", "content": "<a href='/mailto:samrocksc@gmail.com?Subject=shoutinginfrench.com'>Contact</a>"},
             ],
             links2: [
             
             ],
             header: [
-            {"content": "<a href='index.html'><h1>/ s h o u t i n g i n f r e n c h /<img src='images/header.png'/></h1></a>"}
+            {"content": "<a href='/index.html'><h1>/ s h o u t i n g i n f r e n c h /<img src='images/header.png'/></h1></a>"}
             ],
             footer: [ 
-            {"content": "<a href='index.html'>2012 shoutinginfrench</a> || Sam Clark || <a href='mailto:samrocksc@gmail.com?Subject=shoutinginfrench.com'>samrocksc@gmail.com</a>"}
+            {"content": "<a href='/index.html'>2012 shoutinginfrench</a> || Sam Clark || <a href='/mailto:samrocksc@gmail.com?Subject=shoutinginfrench.com'>samrocksc@gmail.com</a>"}
             ],
             social: [
             { "link": "http://www.digg.com/submit", "img": "digg.png"},
